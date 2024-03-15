cba6f048780a51463af0098610bfa79cd25a15d0
ali abedi <ali.abedi9731@yahoo.com>
2024-03-15 04:06:25 +0330

    fix filtering data and fix database query

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,41 +15,39 @@ class HomeController extends Controller
         $api->setAccessToken($token);
 
         $offset = 0;
-        for ($counter = 1; $counter < 33; $counter++) {
+        for ($counter = 1; $counter < 35; $counter++) {
             $data = $api->getPlaylistTracks(config('services.spotify.playlist_id'), [
                 'offset' => $counter * 100,
             ]);
 
-            $tracks = [];
-            $items = response()->json($data)->original->items;
-
-            foreach ($items as $item) {
+            $items = collect(array_map(function ($item) {
 
                 $artists = [];
                 foreach ($item->track->album->artists as $artist) {
                     $artists[] = $artist->name;
                 }
-
-                $tracks[] = [
+                return [
                     'album_id' => $item->track->album->id,
                     'name' => $item->track->album->name,
                     'image_url' => isset($item->track->album->images[0]) ? $item->track->album->images[0]->url : NULL,
                     'artists' => implode(', ', $artists),
                 ];
+            },response()
+                ->json($data)
+                ->original->items));
 
-                $albumIds = Album::all(['album_id'])
-                    ->unique('album_id')
-                    ->pluck('album_id')
-                    ->toArray();
-                Album::insertOrIgnore(
-                    collect($tracks)
-                    ->unique('album_id')
-                    ->whereNotIn('album_id',$albumIds)
-                    ->toArray()
-                );
 
-                sleep(5);
-            }
+            $albumIds = Album::all(['album_id'])
+                ->unique('album_id')
+                ->pluck('album_id')
+                ->toArray();
+
+            Album::insert($items
+                ->unique('album_id')
+                ->whereNotIn('album_id', $albumIds)
+                ->toArray());
+
+            sleep(2);
         }
     }
 }
