5c4bcde2d14c5b2ba9ff83bba7732eebdbfe9ff7
ali abedi <ali.abedi9731@yahoo.com>
2024-03-16 04:25:29 +0330

    update model and migration

--- app/Models/Album.php
+++ app/Models/Album.php
@@ -6,17 +6,19 @@
 
 namespace App\Models;
 
+use App\Enums\Selected;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
  * Class Album
- * 
+ *
  * @property int $id
  * @property string|null $album_id
  * @property string|null $name
  * @property string|null $artists
+ * @property Selected $selected
  * @property string|null $image_url
  * @property Carbon|null $created_at
  * @property Carbon|null $updated_at
@@ -26,13 +28,19 @@ use Illuminate\Database\Eloquent\SoftDeletes;
  */
 class Album extends Model
 {
-	use SoftDeletes;
-	protected $table = 'albums';
+    use SoftDeletes;
 
-	protected $fillable = [
-		'album_id',
-		'name',
-		'artists',
-		'image_url'
-	];
+    protected $table = 'albums';
+
+    protected $fillable = [
+        'album_id',
+        'name',
+        'selected',
+        'artists',
+        'image_url',
+    ];
+
+    protected $casts = [
+        'selected' => Selected::class,
+    ];
 }
